11d1f9f31baa44d1a5e4d2199a9c62c1738d6434
CaiqueFiap-2026 <profcaique.bezerra@fiap.com.br>
2026-01-06 11:19:57 -0300

    Update modulo_teste.py

--- modulo_teste.py
+++ modulo_teste.py
@@ -3,6 +3,10 @@ def soma(a, b):
     '''Soma A e B'''
     return a + b
 
+def subtracao(a, b):
+    '''Subtrai B de A'''
+    return a - b
+
 def multiplicacao(a:float, b:float):
     '''Multiplica A por B'''
     return a * b
